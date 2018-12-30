5696ee1eb8fc9392995839679b67185509d74d11
Benjamin Engelman <Benjamin.Engelman@ulb.ac.be>
2018-12-30 16:06:12 +0100

    update

--- agent.py
+++ agent.py
@@ -4,6 +4,7 @@ from mazes import *
 from helper import *
 import time
 
+
 class AlgoParams():
     def __init__(self, alpha=None, beta=None, gamma=None, temp=None):
         self.ALPHA = alpha
@@ -13,6 +14,61 @@ class AlgoParams():
 
 
 class Agent():
+
+    def update(self, reward, new_state, action):
+        raise NotImplementedError
+
+    def chooseAction(self):
+        raise NotImplementedError
+
+    def learn(self, episodes):
+        last2500RewardIntakes = np.zeros(2500)
+        rewardIntakesEvery2500Episodes = np.zeros(episodes // 2500)
+        allRewardIntakes = np.zeros(50000)
+        numberOfSteps = np.zeros(50000)
+
+        for episodeNum in range(episodes):
+            episodeReward = 0
+            while not self.maze.isDone():
+                action = self.chooseAction()
+                reward, new_state = self.maze.step(action)
+                episodeReward += reward
+                # maze.render()
+                self.update(reward, new_state, action)
+
+            allRewardIntakes[episodeNum] = (
+                episodeReward / maze.actions_counter)
+            numberOfSteps[episodeNum] = maze.actions_counter
+
+            if episodeNum % 2500 == 2499:
+                print("Done %d in %d steps" %
+                      (episodeNum + 1, self.maze.actions_counter))
+                
+                rewardIntakesEvery2500Episodes[episodeNum //
+                                               2500] = (episodeReward / maze.actions_counter)
+
+            if episodeNum + 2500 >= episodes:
+                last2500RewardIntakes[episodeNum -
+                                      47500] = (episodeReward / maze.actions_counter)
+
+            maze.reset()
+
+        return allRewardIntakes, numberOfSteps, last2500RewardIntakes.mean(), rewardIntakesEvery2500Episodes.sum()
+
+
+class AgentWithSingleAlgo(Agent):
+    def __init__(self, maze, algo, params):
+        self.maze = maze
+        self.algo = algo(maze, params)
+    
+    def update(self, reward, new_state, action):
+        self.algo.update(reward, new_state, action)
+
+    def chooseAction(self):
+        return self.algo.getMostProbableAction()
+
+
+class AgentWithEnsemble(Agent):
 
     def __init__(self, maze, ensembleMethod, algoParamsList, temp):
         self.maze = maze
@@ -26,50 +82,26 @@ class Agent():
             ACLA(maze, algoParamsList[4])
         ]
 
-    def learn(self, episodes):
-        last2500RewardIntakes = np.zeros(2500)
-        rewardIntakesEvery2500Episodes = np.zeros(episodes // 2500)
-        allRewardIntakes = np.zeros(50000)
-        numberOfSteps  = np.zeros(50000)
+    def update(self, reward, new_state, action):
+        for algo in self.algos:
+            algo.update(reward, new_state, action)
 
-        for episodeNum in range(episodes):
-            episodeReward = 0
-            while not self.maze.isDone():
-                action = self.ensembleMethod(self.algos, self.temp)
-                reward, new_state = self.maze.step(action)
-                episodeReward += reward
-                # maze.render()
-                                
-                for algo in self.algos:
-                    algo.update(reward, new_state, action)
-                
-            allRewardIntakes[episodeNum] = (episodeReward / maze.actions_counter)
-            numberOfSteps[episodeNum] = maze.actions_counter
-
-            if episodeNum % 2500 == 2499:
-                print("Done %d in %d steps" % (episodeNum + 1, self.maze.actions_counter))
-                print("Reward intake: %s" % (episodeReward / maze.actions_counter))
-                rewardIntakesEvery2500Episodes[episodeNum // 2500] = (episodeReward / maze.actions_counter)
-
-            if episodeNum + 2500 >= episodes:
-                last2500RewardIntakes[episodeNum - 47500] = (episodeReward / maze.actions_counter)
-            
-            maze.reset()
-
-        return allRewardIntakes, numberOfSteps, last2500RewardIntakes.mean(), rewardIntakesEvery2500Episodes.sum()
+    def chooseAction(self):
+        return self.ensembleMethod(self.algos, self.temp)
 
 
 if __name__ == "__main__":
-    parmasList = [
-        AlgoParams(alpha=0.2, gamma=0.9, temp=1),
-        AlgoParams(alpha=0.2, gamma=0.9, temp=1),
-        AlgoParams(alpha=0.1, beta=0.2, gamma=0.95, temp=1),
-        AlgoParams(alpha=0.2, beta=0.2, gamma=0.9, temp=1),
-        AlgoParams(alpha=0.005, beta=0.1, gamma=0.99, temp=1/9)
-    ]
+    paramsDict = {
+        "Q-Learning": AlgoParams(alpha=0.2, gamma=0.9, temp=1),
+        "SARSA": AlgoParams(alpha=0.2, gamma=0.9, temp=1),
+        "Actor-Critic": AlgoParams(alpha=0.1, beta=0.2, gamma=0.95, temp=1),
+        "QV-Learning": AlgoParams(alpha=0.2, beta=0.2, gamma=0.9, temp=1),
+        "ACLA": AlgoParams(alpha=0.005, beta=0.1, gamma=0.99, temp=1/9)
+    }
 
     maze = createSimpleMaze()
-    agent = Agent(maze, majorityVote, parmasList, temp=1/1.6)
+    agent = AgentWithSingleAlgo(
+        maze, QLearning, paramsDict["Q-Learning"])
 
     start = time.time()
 
@@ -81,9 +113,6 @@ if __name__ == "__main__":
     print("Learning process took %d seconds" % (time.time() - start))
 
     print("Final: %s, Cumulative: %s" % (final, cumulative))
-    saveToFile("data.json", [final, cumulative])
-    saveToFile("allRewardsIntakes.json", allRewardIntakes)
-    saveToFile("numberOfSteps.json", numberOfSteps)
-
-    
-    
+    saveToFile("data_QLearning.json", [final, cumulative])
+    saveToFile("allRewardsIntakes_QLearning.json", allRewardIntakes)
+    saveToFile("numberOfSteps_QLearning.json", numberOfSteps)
