13a11eaf25b0666b6491861e97f018644225b8ab
bonaert <harokb@gmail.com>
2019-01-04 15:16:21 +0100

    Update cluster runner

--- clusterRunner.py
+++ clusterRunner.py
@@ -13,11 +13,12 @@ if mode not in ['single', 'ensemble']:
     print("Invalid mode, pass either 'single' or 'ensemble'")
     exit(0)
 
-expNum = sys.argv[2]
+expNum = int(sys.argv[2])
 if expNum not in [3, 4, 5]:
-    print("Invalid mode, pass either 3, 4 or 5")
+    print("Invalid experiment, pass either 3, 4 or 5")
     exit(0)
 
+resultsFilename = "%s-%d.json" % (mode, expNum)
 if expNum == 3:
     algoParams = algoParamsListExp3
     algos = algosExp3
@@ -30,12 +31,13 @@ elif expNum == 4:
     ensembles = ensemblesExp4
     mazeGenerator = createDynamicGoalMaze
 
-
 elif expNum == 5:
     algoParams = algoParamsListExp5
     algos = algosExp5
     ensembles = ensemblesExp5
     mazeGenerator = createGeneralizedMaze
+
+print("Saving results of experiment %d in %s mode in filename %s" % (expNum, mode, resultsFilename))
 
 
 
@@ -46,17 +48,17 @@ def runTrialSingleAlgorithm(algorithm, params, numSteps, maze):
     # Il mesure deux choses
     # 1) Dans 2500 derniers épisodes, fait la moyenne du reward intake
     # 2) Tous les 2500 épisodes, regarde quel est le reward intake, puis à la fin il fait la somme
-    final, cumulative = agent.learn(numSteps)
-    return final, cumulative
+    allRewardIntakes, numberOfSteps = agent.learn(numSteps)
+    return allRewardIntakes, numberOfSteps
 
 def runTrialEnsemble(ensemble, algoParams, temp, numSteps, maze):
     agent = AgentWithEnsemble(maze, ensemble, algoParams, temp, neural=True)
-    final, cumulative = agent.learn(numSteps)
-    return final, cumulative
+    allRewardIntakes, numberOfSteps = agent.learn(numSteps)
+    return allRewardIntakes, numberOfSteps
 
 
 
-def addJobsSingleAlgorithm(jobs, pool, mazeGenerator):
+def addJobsSingleAlgorithm(jobs, pool):
     numSteps = 20000
     for i in range(500):
         maze = mazeGenerator()
@@ -78,7 +80,7 @@ def addJobsEnsemble(jobs, pool):
 
 if mode == 'single':
     results = parallelize(addJobsSingleAlgorithm, numProcesses=32)
-    saveComplexJson("resultsSingleAlgorithm.json", results)
+    saveComplexJson(resultsFilename, results)
 elif mode == 'ensemble':
     results = parallelize(addJobsEnsemble, numProcesses=32)
-    saveComplexJson("resultsEnsemble.json", results)
+    saveComplexJson(resultsFilename, results)
